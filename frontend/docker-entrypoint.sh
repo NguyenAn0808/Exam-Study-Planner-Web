f1d880e0a007edc0ba42c419e8866cb150f65753
NguyenAn0808 <23127148@student.hcmus.edu.vn>
2025-09-29 20:46:42 +0700

    fix: Update frontend URLs to correct Azure Container Apps endpoints
    - Fix hardcoded backend URLs in Dockerfile
    - Update docker-entrypoint.sh with correct internal backend URL
    - This will resolve nginx upstream host not found errors
    - Frontend will now properly connect to backend API

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Thay thế biến môi trường trong nginx config
-export BACKEND_URL=${BACKEND_URL:-https://exam-planner-backend.reddog-2de3819f.eastasia.azurecontainerapps.io}
+export BACKEND_URL=${BACKEND_URL:-https://exam-planner-backend.internal.graywave-05c71fd1.southeastasia.azurecontainerapps.io}
 envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
 mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 
